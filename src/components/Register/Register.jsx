e25f0d7cf1f1425f3a806e1cc7c5195aab93813c
ectionboy <125730248+ectionboy@users.noreply.github.com>
2023-10-28 00:30:46 +0300

    add register styles

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,19 +1,30 @@
-// import { Visibility, VisibilityOff } from '@mui/icons-material';
-// import { Box, Button, FormControl, IconButton, Input, InputAdornment, TextField } from '@mui/material';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  TextField,
+  Typography,
+} from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { signUpThunk } from 'redux/auth/slice';
 import { authSelector } from 'redux/selectors';
 
 const Register = () => {
-  // const [showPassword, setShowPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
 
-  // const handleClickShowPassword = () => setShowPassword((show) => !show);
+  const handleClickShowPassword = () => setShowPassword(show => !show);
 
-  // const handleMouseDownPassword = (event) => {
-  //   event.preventDefault();
-  // };
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
@@ -32,7 +43,6 @@ const Register = () => {
     console.log(password);
     Reg({ name, email, password });
     setPassword('');
-
   };
   const inputName = ({ target: { value } }) => {
     setName(value);
@@ -49,60 +59,96 @@ const Register = () => {
 
   useEffect(() => {
     if (isAuth) {
-
       navigate('/', { replace: true });
-
     }
   }, [isAuth, navigate]);
+  const navToLogPage = () => {
+    navigate('/login', { replace: true });
+  };
+
   return (
-    <div>
-      <h2>Register</h2>
-      <p>You need to register or sign in first</p>
-      <form onSubmit={formSubmit}>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={inputName}
-          required
-        />
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={inputEmail}
-          required
-        />
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={inputPassword}
-          required
-        />
-        <button type="submit">Register</button>
-      </form>
-<div>
-        <p>Already have an account?</p>
-        <Link to="/login">Sign in</Link>
-</div>
-      {/* <FormControl
-      onSubmit={formSubmit}
+    // <div>
+    //   <h2>Register</h2>
+    //   <p>You need to register or sign in first</p>
+    //   <form onSubmit={formSubmit}>
+    //     <input
+    //       type="text"
+    //       id="name"
+    //       value={name}
+    //       onChange={inputName}
+    //       required
+    //     />
+    //     <input
+    //       type="email"
+    //       id="email"
+    //       value={email}
+    //       onChange={inputEmail}
+    //       required
+    //     />
+    //     <input
+    //       type="password"
+    //       id="password"
+    //       value={password}
+    //       onChange={inputPassword}
+    //       required
+    //     />
+    //     <button type="submit">Register</button>
+    //   </form>
+    //   <div>
+    //     <p>Already have an account?</p>
+    //     <Link to="/login">Sign in</Link>
+    //   </div>
+
+    <Container
+      sx={{
+        width: '300px',
+        display: 'flex',
+        flexDirection: 'column',
+        padding: '24px 0',
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Sign up
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom>
+        You need to sign in first
+      </Typography>
+      <Box
+        onSubmit={formSubmit}
         component="form"
         sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
+          width: '100%',
           display: 'flex',
           flexDirection: 'column',
+          gap: '14px',
         }}
       >
-        <TextField required id="name" label="Name" variant="standard" type="text" value={name} onChange={inputName} />
-        <TextField required id="email" label="Email" variant="standard" type="email" value={email} onChange={inputEmail} />
-        <Input
-        required
+        <TextField
+          value={name}
+          onChange={inputName}
+          id="name"
+          label="Name"
+          variant="standard"
+          type="text"
+          required
+        />
+        <TextField
+          value={email}
+          onChange={inputEmail}
+          id="email"
+          label="Email"
+          variant="standard"
+          type="email"
+          required
+        />
+        <FormControl variant="standard">
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input
+            value={password}
+            onChange={inputPassword}
             id="password"
             type={showPassword ? 'text' : 'password'}
-            value={password}
-            onChange={inputPassword} 
+            required
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -115,11 +161,35 @@ const Register = () => {
               </InputAdornment>
             }
           />
+        </FormControl>
+
         <Button type="submit" variant="contained">
-          Login
+          Register
         </Button>
-      </FormControl> */}
-    </div>
+      </Box>
+      <Typography
+        variant="subtitle2"
+        sx={{
+          maxWidth: '180px',
+          margin: '0 auto',
+          marginTop: '16px'
+        }}
+      >
+        Already have an account?
+      </Typography>
+      <Button
+        sx={{
+          maxWidth: '180px',
+          margin: '0 auto',
+        }}
+        onClick={navToLogPage}
+        type="button"
+        size="small"
+      >
+        Sign in
+      </Button>
+    </Container>
+    // </div>
   );
 };
 
