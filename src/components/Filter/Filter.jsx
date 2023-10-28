486a7a9bbd47708065a3ff4fb33d22554bc05af7
ectionboy <125730248+ectionboy@users.noreply.github.com>
2023-10-29 02:35:12 +0300

    add Filter styles

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,30 +1,40 @@
-import React from 'react'
-import {  useDispatch, useSelector } from 'react-redux';
-import { FilterContainer, FilterLabel, FilterInput } from './Filter.styled';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getFilter } from 'redux/selectors';
 import { filterSet } from 'redux/filter/slice';
-
+import { Box, TextField, Typography } from '@mui/material';
 
 const Filter = () => {
+  const filter = useSelector(getFilter);
+  const dispatch = useDispatch();
 
-    const filter = useSelector(getFilter);
-    const dispatch = useDispatch();
-  
-    const handleFilter = value => {
-      dispatch(filterSet(value));
-    }
+  const handleFilter = value => {
+    dispatch(filterSet(value));
+  };
   return (
-    <FilterContainer>
-      <FilterLabel htmlFor="contactsfilter">Find contacts by name</FilterLabel>
-      <FilterInput
-        type="text"
-        name="filter"
-        id="contactsfilter"
+    // <FilterContainer>
+    //   <FilterLabel htmlFor="contactsfilter">Find contacts by name</FilterLabel>
+    //   <FilterInput
+    //     type="text"
+    //     name="filter"
+    //     id="contactsfilter"
+    //     value={filter ?? ''}
+    //     onChange={value => handleFilter(value.target.value)}
+    //   />
+    // </FilterContainer>
+    <Box sx={{marginTop: '24px'}}>
+      <Typography variant="h6" gutterBottom>
+      Find contacts by name
+      </Typography>
+      <TextField
+        id="contactsfilter-basic"
+        variant="outlined"
+        size="small"
         value={filter ?? ''}
         onChange={value => handleFilter(value.target.value)}
       />
-    </FilterContainer>
-  )
-}
+    </Box>
+  );
+};
 
-export default Filter
+export default Filter;
