96d772eef4a31092607d333c9cefbb8d9f9cc070
ectionboy <125730248+ectionboy@users.noreply.github.com>
2023-10-30 21:48:08 +0200

    del location

--- src/guards/PrivateRoute/PrivateRoute.jsx
+++ src/guards/PrivateRoute/PrivateRoute.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { authSelector } from 'redux/selectors';
 
 const PrivateRoute = ({ children }) => {
   const isAuth = useSelector(authSelector);
-  const location = useLocation();
 
-  return isAuth ? children : <Navigate to="/login" state={location} />;
+  return isAuth ? children : <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
--- src/guards/PublicRoute/PublicRoute.jsx
+++ src/guards/PublicRoute/PublicRoute.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { authSelector } from 'redux/selectors';
 
 const PublicRoute = ({ children }) => {
   const isAuth = useSelector(authSelector);
-  const location = useLocation();
 
   //   return isAuth ? children : <Navigate to='/' state={location} />
-  return !isAuth ? children : <Navigate to="/" state={location} />;
+  return !isAuth ? children : <Navigate to="/" />;
 };
 
 export default PublicRoute;
